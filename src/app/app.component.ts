f55145a3da8220de6f09927c7509a6354b800d73
brogli <alexanderbrogli@gmx.net>
2022-11-04 12:28:54 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -13,7 +13,7 @@ export class AppComponent {
         console.log('somestring');
     }
 
-    public getSomeString(): string {
+    public getSomeString() {
         return 'someotherstring';
     }
 }
