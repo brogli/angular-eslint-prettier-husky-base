591f14de761285c031e0398dea0659afacf0c19a
brogli <28339986+brogli@users.noreply.github.com>
2022-11-04 13:25:21 +0100

    Various manual tests and improvements (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-eslint-prettier-husky-base';
+    title = 'angular-eslint-prettier-husky-base';
+    somevariable: any;
+
+    x = 1;
+
+    public someVoidMethod(): void {
+        console.log('somestring');
+    }
+
+    public getSomeString(): string {
+        return 'someotherstring';
+    }
 }
